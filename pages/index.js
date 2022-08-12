891bb8ec0887864bed61c2a716845c1fa47e7497
Valeskitax <52110505+Valeskitax@users.noreply.github.com>
2022-08-11 21:43:43 -0400

    corregir error

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
 
       <main>
         <h1 className="title">
-          Bienvenid@ a mi páginatps!</a>
+          Bienvenid@ a mi páginatps!
         </h1>
 
         <p className="description">
